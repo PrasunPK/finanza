7b11b01bb972a656c1335ee02a1f7c1895251a4f
Prasun <prasun_1993@rediffmail.com>
2016-08-16 10:26:23 +0530

    spiking on DB migration

---
+++ migrations/sqls/20160816040021-tesing-features-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+DROP TABLE test_table_1;
---
+++ migrations/sqls/20160816040021-tesing-features-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE test_table_1 (name varchar(10), phone numeric(15));
