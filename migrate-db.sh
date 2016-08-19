3bdcd890092eb56490c46de6ba5f4c96aa7a76d9
Prasun <prasun_1993@rediffmail.com>
2016-08-19 10:55:20 +0530

    [Spike] fixing migration script

--- migrate-db.sh
+++ migrate-db.sh
@@ -1,7 +1,10 @@
-mkdir -p /tmp
 rm /tmp/database.json
 
+
 DB_ROOT_CONFIG_FILE="./app-root/repo/database.json"
+DATABASE_CONFIG_FILE="/tmp/database.json"
+
+cp "${DB_ROOT_CONFIG_FILE}"  "${DATABASE_CONFIG_FILE}"
 
 if [ -f "$DB_ROOT_CONFIG_FILE" ]; then
 	PRODUCTION_MIGRATION_DIR="./app-root/repo/migrations"
@@ -9,6 +12,5 @@ else
 	PRODUCTION_MIGRATION_DIR="./migrations"
 fi
 
-DATABASE_CONFIG_FILE="/tmp/database.json"
 
 db-migrate up "--config ${DATABASE_CONFIG_FILE} --migrations-dir ${PRODUCTION_MIGRATION_DIR}  -e ${NODE_ENVIRONMENT}"
